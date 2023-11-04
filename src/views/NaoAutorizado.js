ed5115f4fb26f1be916d8080dadcf3064d52dea5
Eliton Souza <ess3@icomp.ufam.edu.br>
2023-11-04 12:46:15 -0400

    criando pagina para nao autorizado

---
+++ src/views/NaoAutorizado.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NaoAutorizado = () => { 
+
+  return (
+    <>
+      <h1>
+        VOCÊ NÃO TEM AUTORIZAÇÃO PARA ACESSAR ESTA PÁGINA!
+      </h1>
+
+      <h1>
+        PROCURE UM SECRETÁRIO
+      </h1>
+    </>
+  );
+};
+
+export default NaoAutorizado;
